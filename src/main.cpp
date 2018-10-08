5599069378409e2e510e54253be69fecfd24c09c
Bert Melis <bertmelis@users.noreply.github.com>
2018-10-08 23:06:30 +0200

    update to new TreeLight

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,17 @@ static const IPAddress BROKER(192, 168, 1, 2);
 static const uint16_t PORT =  1883;
 
 Ticker VitoWiFiTimer;
-Ticker StatsTimer;
 volatile bool updateVitoWiFi = false;
+#if USE_STATS
+Ticker StatsTimer;
+#endif
 volatile bool updateStats = false;
 
 // datapoint names and groups list
 const char outsideTempName[] = "outsideTemp";
 const char boilerTempName[] = "boilerTemp";
 const char dhwTempName[] = "dhwTemp";
-const char dhwSolerTempName[] = "dhwSolarTemp";
+const char dhwSolarTempName[] = "dhwSolarTemp";
 const char dhwSolarCollTempName[] = "dhwSolarCollTemp";
 const char solarPumpStatName[] = "solarPumpStat";
 const char solarPumpHoursName[] = "solarPumpHours";
@@ -35,27 +37,24 @@ VitoWiFi_setProtocol(P300);  // this also initializes VitoWiFi.
 DPTemp outsideTemp(outsideTempName, boilerGroup, 0x5525);
 DPTemp boilerTemp(boilerTempName, boilerGroup, 0x0810);
 DPTemp dhwTemp(dhwTempName, hotwaterGroup, 0x0812);
-DPTemp dhwSolarTemp(dhwSolerTempName, hotwaterGroup, 0x6566);
+DPTemp dhwSolarTemp(dhwSolarTempName, hotwaterGroup, 0x6566);
 DPTemp dhwSolarCollTemp(dhwSolarCollTempName, hotwaterGroup, 0x6564);
 DPStat solarPumpStat(solarPumpStatName, hotwaterGroup, 0x6552);
 DPCountS solarPumpHours(solarPumpHoursName, hotwaterGroup, 0x6568);
 
 // TreeLightNodes initialization
-TreeLightNode boilerTempNode(boilerTempName, false, NUMBER);
-TreeLightNode outsideTempNode(outsideTempName, false, NUMBER);
-TreeLightNode dhwTempNode(dhwTempName, false, NUMBER);
-TreeLightNode dhwSolarTempNode(dhwSolerTempName, false, NUMBER);
-TreeLightNode dhwSolarCollTempNode(dhwSolarCollTempName, false, NUMBER);
-TreeLightNode solarPumpStatNode(solarPumpStatName, false, NUMBER);
-TreeLightNode solarPumpHoursNode(solarPumpHoursName, false, NUMBER);
+FloatNode outsideTempNode(outsideTempName, false);
+FloatNode boilerTempNode(boilerTempName, false);
+FloatNode dhwTempNode(dhwTempName, false);
+FloatNode dhwSolarTempNode(dhwSolarTempName, false);
+FloatNode dhwSolarCollTempNode(dhwSolarCollTempName, false);
+BoolNode solarPumpStatNode(solarPumpStatName, false);
+IntNode solarPumpHoursNode(solarPumpHoursName, false);
 
 void globalCallbackHandler(const IDatapoint& dp, DPValue value) {
-  TreeLightNode* n = TreeLight.findNode(dp.getName());
-  if (n) {
-    char str[9] = {"\0"};
-    value.getString(str, sizeof(str));
-    TreeLight.setNode(*n, str);
-  }
+  char str[9] = {"\0"};
+  value.getString(str, sizeof(str));
+  TreeLight.printf("node %s: %str\n", dp.getName(), str);
 }
 
 void setup() {
@@ -67,9 +66,34 @@ void setup() {
   VitoWiFiTimer.attach(60, [](){
     updateVitoWiFi = true;
   });
-  StatsTimer.attach(10, [](){
+
+  outsideTemp.setCallback([](const IDatapoint& dp, DPValue value){
+    outsideTempNode.setValue(value.getFloat());
+  });
+  boilerTemp.setCallback([](const IDatapoint& dp, DPValue value){
+    boilerTempNode.setValue(value.getFloat());
+  });
+  dhwTemp.setCallback([](const IDatapoint& dp, DPValue value){
+    dhwTempNode.setValue(value.getFloat());
+  });
+  dhwSolarTemp.setCallback([](const IDatapoint& dp, DPValue value){
+    dhwSolarTempNode.setValue(value.getFloat());
+  });
+  dhwSolarCollTemp.setCallback([](const IDatapoint& dp, DPValue value){
+    dhwSolarCollTempNode.setValue(value.getFloat());
+  });
+  solarPumpStat.setCallback([](const IDatapoint& dp, DPValue value){
+    solarPumpStatNode.setValue(value.getBool());
+  });
+  solarPumpHours.setCallback([](const IDatapoint& dp, DPValue value){
+    solarPumpHoursNode.setValue(value.getU16());
+  });
+
+#if USE_STATS
+  StatsTimer.attach(60, [](){
     updateStats = true;
   });
+#endif
 
   TreeLight.setHostname("HEATING");
   TreeLight.setupWiFi(SSID, PASS);
@@ -85,8 +109,10 @@ void loop() {
     updateVitoWiFi = false;
     VitoWiFi.readAll();
   }
+#if USE_STATS
   if (updateStats) {
     updateStats = false;
     TreeLight.updateStats();
   }
+#endif
 }
